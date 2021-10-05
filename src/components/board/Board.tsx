56582e41aba96d8fb62b02aa12af0daf9ab2ad40
Ilya Zaichenko <dai.fudo@mail.ru>
2021-10-05 18:12:12 +0300

    добавлена логика с либы bdnd

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 import { v4 as uuidv4 } from "uuid";
 
@@ -27,9 +27,54 @@ interface ICard {
   idInput: string;
   task: { name: string; key: string }[];
 }
+/* interface IResDrag {
+  result: any;
+  boards: any;
+  setBoards: any;
+} */
 
 const Board: React.FC = () => {
   const [cards, setCards] = useState<ICard[]>([]);
+
+  // drag and drop
+  const onDragEnd = (result: any, boards: any, setBoards: any) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+
+    if (source.droppableId !== destination.droppableId) {
+      const sourceBoard = boards[source.droppableId];
+      const destBoard = boards[destination.droppableId];
+      const sourceItems = [...sourceBoard.items];
+      const destItems = [...destBoard.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setCards({
+        ...boards,
+        [source.droppableId]: {
+          ...sourceBoard,
+          items: sourceItems,
+        },
+        [destination.droppableId]: {
+          ...destBoard,
+          items: destItems,
+        },
+      });
+    } else {
+      const board = boards[source.droppableId];
+      const copiedItems = [...board.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setCards({
+        ...boards,
+        [source.droppableId]: {
+          ...board,
+          items: copiedItems,
+        },
+      });
+    }
+  };
+
+  //
 
   const createBoard = async (e: EventType) => {
     let boardTitle = e.target.value;
@@ -83,18 +128,46 @@ const Board: React.FC = () => {
       console.log(item);
 
       return (
-        <Card key={item.idForm}>
-          <Title>{item.title}</Title>
-          <List>
-            {item.task?.map((task) => (
-              <Item component="a" key={task.key}>
-                {task.name}
-                <DeleteIcon />
-              </Item>
-            ))}
-          </List>
-          {renderInputForTask()}
-        </Card>
+        <DragDropContext
+          onDragEnd={(result) => onDragEnd(result, cards, setCards)}
+        >
+          <Card key={item.idForm}>
+            <Title>{item.title}</Title>
+            <List>
+              <Droppable droppableId={item.idForm}>
+                {(provided, snapshot) => {
+                  return (
+                    <div {...provided.droppableProps} ref={provided.innerRef}>
+                      {item.task.map((task, index) => {
+                        return (
+                          <Draggable
+                            key={task.key}
+                            draggableId={task.key}
+                            index={index}
+                          >
+                            {(provided, snapshot) => {
+                              return (
+                                <div
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  {task.name}
+                                </div>
+                              );
+                            }}
+                          </Draggable>
+                        );
+                      })}
+                      {provided.placeholder}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            </List>
+            {renderInputForTask()}
+          </Card>
+        </DragDropContext>
       );
     });
   };
